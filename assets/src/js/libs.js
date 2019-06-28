b9acc0e7f8459092d9525cb5b7acaf56e8a91aa1
vigolajnen.anna <vigolajnen@gmail.com>
2019-06-28 18:14:54 +0300

    добавила foreach-polyfill

--- assets/src/js/libs.js
+++ assets/src/js/libs.js
@@ -1,2 +1,3 @@
+//= ../../../node_modules/foreach-polyfill/dist/array-foreach-polyfill.js
 //= ../../../node_modules/picturefill/dist/picturefill.js
 //= ../../../node_modules/svg4everybody/dist/svg4everybody.js
