0918bfdd0d8d270a9123ad63afcd89f8dc518a62
vigolajnen.anna <vigolajnen@gmail.com>
2019-07-24 18:16:41 +0300

    tabs

--- assets/src/js/main/tabs-2.js
+++ assets/src/js/main/tabs-2.js
@@ -1,3 +1,24 @@
+// (function() {
+//   var jsTriggers = document.querySelectorAll(".js-tab-trigger");
+//   jsTriggers.forEach(function(trigger) {
+//     trigger.addEventListener("click", function() {
+//       var id = this.getAttribute("data-tab"),
+//         content = document.querySelector(
+//           '.js-tab-content[data-tab="' + id + '"]'
+//         ),
+//         activeTrigger = document.querySelector(".js-tab-trigger.active"),
+//         activeContent = document.querySelector(".js-tab-content.active");
+
+//       activeTrigger.classList.remove("active");
+//       trigger.classList.add("active");
+
+//       activeContent.classList.remove("active");
+//       content.classList.add("active");
+//     });
+//   });
+// })();
+
+
 (function() {
   var items = document.querySelectorAll(".tabs__item");
   var contents = document.querySelectorAll(".tabs__panel");
