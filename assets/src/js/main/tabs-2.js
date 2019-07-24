f5c89b3f759f898a5c287e31b9d29ec9d953eed0
vigolajnen.anna <vigolajnen@gmail.com>
2019-07-24 16:58:37 +0300

    табы -исправила баг с открытием двух вкладок

--- assets/src/js/main/tabs-2.js
+++ assets/src/js/main/tabs-2.js
@@ -1,24 +1,32 @@
 (function() {
-  var jsTriggers = document.querySelectorAll(".js-tab-trigger");
-  jsTriggers.forEach(function(trigger) {
-    trigger.addEventListener("click", function() {
-      var id = this.getAttribute("data-tab"),
-        content = document.querySelector(
-          '.js-tab-content[data-tab="' + id + '"]'
-        ),
-        activeTrigger = document.querySelector(".js-tab-trigger.active"),
-        activeContent = document.querySelector(".js-tab-content.active");
+  var items = document.querySelectorAll(".tabs__item");
+  var contents = document.querySelectorAll(".tabs__panel");
 
-      activeTrigger.classList.remove("active");
-      trigger.classList.add("active");
+  items.forEach(function(item) {
+    item.addEventListener("click", function(evt) {
+      evt.preventDefault();
 
-      activeContent.classList.remove("active");
-      content.classList.add("active");
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove("active");
+      }
+      for (var i = 0; i < contents.length; i++) {
+        contents[i].classList.remove("active");
+      }
+
+      var tabName = this.getAttribute("data-tab");
+
+      var activeItem = document.querySelector(
+        ".tabs__item[data-tab='" + tabName + "']"
+      );
+      var activeContent = document.querySelector(
+        ".tabs__panel[data-tab='" + tabName + "']"
+      );
+
+      activeItem.classList.add("active");
+      activeContent.classList.add("active");
     });
   });
-})();
 
-(function() {
   var jsTriggersSub = document.querySelectorAll(".js-subtab-trigger");
   jsTriggersSub.forEach(function(trigger) {
     trigger.addEventListener("click", function() {
