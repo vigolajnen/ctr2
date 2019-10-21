e271918e4122f4cd940fb62cf718e566c86ae28b
vigolajnen.anna <vigolajnen@gmail.com>
2019-10-21 17:59:30 +0300

    убрала стили бутстрапа из libs + правка типографики для контента

--- assets/src/js/libs.js
+++ assets/src/js/libs.js
@@ -1,9 +1,3 @@
 //= ../../../node_modules/foreach-polyfill/dist/array-foreach-polyfill.js
 //= ../../../node_modules/picturefill/dist/picturefill.js
 //= ../../../node_modules/svg4everybody/dist/svg4everybody.js
-
-// Импортируем jQuery
-//= ../../../node_modules/jquery/dist/jquery.js
-
-// Импортируем необходимые js-файлы Bootstrap 4
-//= ../../../node_modules/bootstrap/dist/js/bootstrap.js
